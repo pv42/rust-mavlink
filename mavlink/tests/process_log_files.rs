54453c32b4b351d4e30b0b5e4a1a04d017d3e5dc
pv42 <pv42.97@gmail.com>
2025-11-26 22:54:09 +0100

    add read bench

--- mavlink/tests/process_log_files.rs
+++ mavlink/tests/process_log_files.rs
@@ -1,3 +1,7 @@
+#![feature(test)]
+
+extern crate test;
+
 #[cfg(all(feature = "default", feature = "ardupilotmega"))]
 mod process_files {
     use mavlink::ardupilotmega::MavMessage;
@@ -52,5 +56,45 @@ mod process_files {
             counter == 1426,
             "Unable to hit the necessary amount of matches"
         );
+    }
+
+    use std::io::Read;
+    use test::Bencher;
+
+    // test process_files::bench_read_any_msg ... bench: 230,560,580.00 ns/iter (+/- 1,034,277.00)
+    // test process_files::bench_read_any_msg ... bench: 229,439,510.00 ns/iter (+/- 943,793.00)
+
+    //
+    // 231,425.00 ns/iter (+/- 2,482.50)
+    // 232,786.67 ns/iter (+/- 2,487.33)
+    #[bench]
+    fn bench_read_any_msg(bench: &mut Bencher) {
+        // Get path for download script
+        let tlog = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("tests/log.tlog")
+            .canonicalize()
+            .unwrap();
+
+        let tlog = tlog.to_str().unwrap();
+
+        let filename = std::path::Path::new(tlog);
+
+        let mut file = std::fs::File::open(filename).unwrap();
+        let mut buf = Vec::with_capacity(file.metadata().unwrap().len() as usize);
+        file.read_to_end(&mut buf).unwrap();
+
+        bench.iter(|| process_data(&buf));
+    }
+
+    fn process_data(data: &[u8]) {
+        use mavlink::peek_reader::PeekReader;
+        let mut reader = PeekReader::new(data);
+        let mut counter = 0;
+        while let Ok((head, body)) = mavlink::read_any_msg::<MavMessage, _>(&mut reader) {
+            std::hint::black_box(head);
+            std::hint::black_box(body);
+            counter += 1;
+        }
+        assert_eq!(counter, 1426);
     }
 }
